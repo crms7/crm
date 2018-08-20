bc62a93f2dbc257269eac2c9040d2433258cda9c
nianxin-tao <18727865658@163.com>
2018-08-20 18:33:20 +0800

    zt

--- crm/src/main/java/com/crm/controller/EmployeeInfo/EmployeeInfoController.java
+++ crm/src/main/java/com/crm/controller/EmployeeInfo/EmployeeInfoController.java
@@ -5,6 +5,8 @@ import com.crm.entity.EmployeeInfo;
 import com.crm.service.EmployeeInfo.EmployeeInfoService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
@@ -13,6 +15,7 @@ import javax.servlet.http.HttpSession;
 public class EmployeeInfoController {
     @Resource
     EmployeeInfoService employeeInfoService;
+    ModelAndView mv = new ModelAndView();
 
     /**
      * 登录信息验证
@@ -20,25 +23,31 @@ public class EmployeeInfoController {
      * @param session
      * @return
      */
-    @RequestMapping("/loginTo")
-    public String login(EmployeeInfo employeeInfo, HttpSession session){
-        String m_pwd = MD5.MD5Util(employeeInfo.getE_Pwd()); //加密后密码
-        employeeInfo.setE_Pwd(m_pwd);
-        String msg=null;
-        EmployeeInfo emp=null;
-        try {
-            emp = employeeInfoService.login(employeeInfo);
-        } catch (Exception e) {
-            System.out.println(e);
-        }finally {
-            if(emp!=null){
-                session.setAttribute("emp",emp);
-                return "index";
-            }else{
-                msg="账号或密码错误";
-                return "page-login";
+    @RequestMapping(value = "/loginTo",method =  RequestMethod.POST)
+    public ModelAndView login(EmployeeInfo employeeInfo, HttpSession session){
+        //判断接收页面密码是否为空，为空直接返回登录
+        if(employeeInfo.getE_Pwd()!=null&&employeeInfo.getE_Pwd()!=""){
+            String m_pwd = MD5.MD5Util(employeeInfo.getE_Pwd()); //加密后密码
+            employeeInfo.setE_Pwd(m_pwd);
+            EmployeeInfo emp=null;
+            try {
+                emp = employeeInfoService.login(employeeInfo);
+            } catch (Exception e) {
+                System.out.println(e);
+            }finally {
+                if(emp!=null){
+                    session.setAttribute("emp",emp);
+                    mv.setViewName("index");
+                }else{
+                    mv.addObject("msg","账号或密码错误");
+                    mv.setViewName("page-login");
+                }
             }
+        }else{
+            mv.addObject("msg","账号和密码不能为空");
+            mv.setViewName("page-login");
         }
+        return mv;
     }
 
     //测试 后面删掉
